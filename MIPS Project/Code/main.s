a9c33cea096accec511e87960bf4fd6bc6735471
mabushelbaia <mabushelbaia@gmail.com>
2022-12-17 15:15:00 +0200

    Read from file done

--- MIPS Project/Code/main.s
+++ MIPS Project/Code/main.s
@@ -1,7 +1,8 @@
 .data
 filename: .space 30
+file: .asciiz "input.txt"
 message: .asciiz "Enter file name: \n"
-
+contents: .space 1048576
 .text
 .globl main
 main:
@@ -15,6 +16,23 @@ la $a0, filename
 li $v0, 8
 syscall
 # Re-printing the filename to assure that we got it right.
+li $v0, 4
+syscall
+# Reading From a file.
+li $v0, 13
+la $a0, file			# Here the problem is that we need to make "filename" null terminated
+la $a1, 0
+la $a2, 0
+syscall
+
+# Reading the file contents.
+move $a0, $v0               # $a0 is the file descriptor.
+la $a1, contents            # $a1 is the buffer.
+li $a2, 1048576           # $a2 is the size of the buffer.
+li $v0, 14
+syscall
+#Printing The file contents.
+move $a0, $a1
 li $v0, 4
 syscall
 # exiting the program
